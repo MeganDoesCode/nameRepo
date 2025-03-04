d8703f60ebf8b5625272837e0decf32739de3b10
Megan Jensen <spottedeye2@gmail.com>
2025-03-04 13:50:10 -0600

    Added printName function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void printName();
+
 int main()
 {
     cout<<"GitHub Demo"<<endl;
+    printName();
     return 0;
+}
+
+void printName()
+{
+    cout<<"Megan Jensen"<<endl;
 }
